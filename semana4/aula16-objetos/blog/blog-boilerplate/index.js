4575b346ff8a827c71ebc16b69dc22a064eb20d7
lausmpc <lauranunesc.@gmail.com>
2021-04-28 16:01:26 -0300

    feito o desafio 1

--- semana4/aula16-objetos/blog/blog-boilerplate/index.js
+++ semana4/aula16-objetos/blog/blog-boilerplate/index.js
@@ -3,23 +3,25 @@ function criarPost() {
   const titulo = document.getElementById("titulo-post");
   const autor = document.getElementById("autor-post");
   const conteudo = document.getElementById("conteudo-post");
+  const imagem = document.getElementById("imagem-post")
   
   const post = {
     titulo: titulo.value,
     autor: autor.value,
     conteudo: conteudo.value,
+    imagem: imagem.value,
   };
 
   arrayDePosts.push(post);
-  console.log(arrayDePosts)
-
+  
   const containerDePosts = document.getElementById("container-de-posts")
-  containerDePosts.innerHTML += `<p>${post.titulo}</p> <p>${post.autor}</p> <p>${post.conteudo}</p>`
-
+  containerDePosts.innerHTML += `<p>${post.titulo}</p> <p>${post.autor}</p> <p>${post.conteudo}</p> 
+  <img src="${imagem}">`
 
   titulo.value = "";
   autor.value = "";
   conteudo.value = "";
+  imagem.value = "";
 }
 
 

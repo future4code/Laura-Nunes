6a8438b6de97a76c33f42429ae0b1bd1beb8d513
lausmpc <lauranunesc.@gmail.com>
2021-05-20 14:36:29 -0300

    Projeto Labefy criado

---
+++ semana7/projeto/labefy/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+
+export default class App extends React.Components {
+
+  render () {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+  
+
+

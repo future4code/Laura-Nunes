42a4b2d1b573f875648120edd0ee23253708c388
unknown <lauranunes.c@gmail.com>
2021-07-13 22:35:46 -0300

    criada pasta de backend com exercícios da aula 42

---
+++ backend/aula42-introducao-typescript/src/ex2.ts
@@ -0,0 +1,21 @@
+// A) 
+function obterEstatisticas(numeros: number) {
+
+    const numerosOrdenados = numeros.sort(
+        (a: number, b: number) => a - b
+    )
+
+    let soma = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
